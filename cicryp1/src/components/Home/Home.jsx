952367b7a5a089139549a41cffb73cc35a4c39fc
aslanovgx <aslanov7509@gmail.com>
2023-01-20 01:07:31 +0400

    Navbar and #discovery section are completed

---
+++ cicryp1/src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Discover from "./Discover/Discover";
+
+const Home = () => {
+  return (
+    <main>
+      <Discover />
+    </main>
+  );
+};
+
+export default Home;
---
+++ cicryp1/src/components/Layout/Navbar/Navbar.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import styled from "./../Navbar/Navbar.module.css";
+import "./../Navbar/Navbar.css";
+import { FiSearch } from "react-icons/fi";
+import { BiChevronDown } from "react-icons/bi";
+import { BsMoon } from "react-icons/bs";
+
+const Navbar = () => {
+  return (
+    <nav id={styled.nav}>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">
+            <div className={styled.nav_left}>
+              <img
+                src="https://chisnghiax.com/ciscryp/static/media/logo-light.efd6c8c15ce9ff0f90cc7add4d464ab1.svg"
+                alt=""
+              />
+              <form action="#">
+                <input type="text" name="" id="" placeholder="Search items" />
+                <span>
+                  <FiSearch />
+                </span>
+              </form>
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <ul className={styled.nav_right}>
+              <li>
+                <a href="#">
+                  Discover
+                  <BiChevronDown />
+                </a>
+              </li>
+              <li>
+                <a href="#">Help center</a>
+              </li>
+              <li>
+                <a href="#">
+                  <BsMoon />
+                </a>
+              </li>
+              <li>
+                <a href="#">Create</a>
+              </li>
+              <li>
+                <a href="#">Connect Wallet</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
